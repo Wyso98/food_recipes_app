b6faa4f1796bd72a31f3e37206f1045099aedcfe
Karol <karol98wyso@gmail.com>
2023-10-02 21:25:42 +0200

    I fixed my code, sorted and changed some files. I used infinite scrall in SearchPage.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,12 @@
 import React from "react";
 import {useEffect, useState} from 'react'
-import {getData} from "./API/API-metods.jsx";
 import {HashRouter, Route, Routes} from "react-router-dom";
 import {EntryPage} from "./EntryPage.jsx";
 import {Navigation} from "./Navigation.jsx";
-import {BrowserPage} from "./BrowserPage.jsx";
-import InfiniteScroll from "react-infinite-scroller";
+import {SearchPage} from "./SearchPage.jsx";
 
 function App() {
-
     const [arrayOfRecipes, setArrayOfRecipes] = useState([]);
-    const [formSearch, setFormSearch] = useState("")
-    const [message, setMessage] = useState("There you will se results of your search")
-    const [nextLink, setNextLink] = useState("")
-
-    const searchForRecipeInput = (event)=>{
-        setFormSearch(event.target.value)
-    }
-
-    const runBrowser = (event) =>{
-        event.preventDefault()
-        setMessage("Wait for your data")
-        getData(setArrayOfRecipes, formSearch, setNextLink)
-    }
 
   return (
           <HashRouter>
@@ -30,7 +14,7 @@ function App() {
               <div className={"pageBackground"}>
                   <Routes>
                       <Route path={"/"} element={<EntryPage/>}/>
-                      <Route path={"/searchForRecipe"} element={<BrowserPage arrayOfRecipes={arrayOfRecipes} formSearch={formSearch} message={message} searchForRecipeInput={searchForRecipeInput} runBrowser={runBrowser}/>}/>
+                      <Route path={"/searchForRecipe"} element={<SearchPage arrayOfRecipes={arrayOfRecipes} setArrayOfRecipes={setArrayOfRecipes}/>}/>
                   </Routes>
               </div>
           </HashRouter>
