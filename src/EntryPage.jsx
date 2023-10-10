37599434c36c18ccdb2c2017640b47081b0ff721
Karol <karol98wyso@gmail.com>
2023-10-10 22:06:57 +0200

    I made responsive navidation with Hambutger and chenged chef image on enteryPage.

--- src/EntryPage.jsx
+++ src/EntryPage.jsx
@@ -8,7 +8,7 @@ export const EntryPage = ()=>{
                     <header className={"header"}>
                         <div className={"header_box"}>
                             <h2 className={"display-3 header_text"}>Hello flavor seeker. Look for recipes that will delight you and your guests. Happy hunting! </h2>
-                            <img className={"header_image"} src={"../public/chef.png"} alt={"chef-image"}/>
+                            <img className={"header_image"} src={"../public/chefImage.svg"} alt={"chef-image"}/>
                         </div>
                     </header>
                     <section>
--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import {NavLink} from "react-router-dom";
 
 export const Navigation = ()=>{
+    const [showNavForHamburger, setShowNavForHamburger] = useState(false)
+
     return(
         <nav className="navigation-container">
             <div className="container navigation-box">
@@ -10,6 +12,13 @@ export const Navigation = ()=>{
                     <NavLink className={({isActive}) => `nav_element ${isActive ? "active_element": undefined}`} end to={"/"}  >Strona główna</NavLink>
                     <NavLink className={({isActive}) => `nav_element ${isActive ? "active_element": undefined}`} end to={"/searchForRecipe"}>Wyszukiwarka</NavLink>
                 </div>
+                    <img onClick={()=>setShowNavForHamburger(!showNavForHamburger)} className={"nav-hamburger-image"} src={"../public/bars-solid.svg"} alt={"menu"}/>
+                {showNavForHamburger ?
+                    <div className={"nav-forHamburger"}>
+                    <NavLink className={({isActive}) => `nav-forHamburger-element ${isActive ? "active_element": undefined}`} end to={"/"}  >Strona główna</NavLink>
+                    <NavLink className={({isActive}) => `nav-forHamburger-element ${isActive ? "active_element": undefined}`} end to={"/searchForRecipe"}>Wyszukiwarka</NavLink>
+                </div>
+                : ""}
             </div>
         </nav>
     )
